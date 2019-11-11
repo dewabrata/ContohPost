36b1fc2dad1f8ad6f16c2872fed9f5ccf93935e9
Dewa <dewabrata@gmail.com>
2019-11-11 14:31:26 +0700

    update buka folder image

--- app/src/main/java/com/juaracoding/contohpost/PostActivity.java
+++ app/src/main/java/com/juaracoding/contohpost/PostActivity.java
@@ -10,12 +10,18 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 
+import com.github.angads25.filepicker.controller.DialogSelectionListener;
+import com.github.angads25.filepicker.model.DialogConfigs;
+import com.github.angads25.filepicker.model.DialogProperties;
+import com.github.angads25.filepicker.view.FilePickerDialog;
 import com.otaliastudios.cameraview.BitmapCallback;
 import com.otaliastudios.cameraview.CameraListener;
 import com.otaliastudios.cameraview.CameraView;
 import com.otaliastudios.cameraview.PictureResult;
 import com.otaliastudios.cameraview.filter.Filters;
+import com.squareup.picasso.Picasso;
 
+import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Locale;
@@ -27,7 +33,7 @@ public class PostActivity extends AppCompatActivity {
     EditText txtDari,txtTime,txtText;
     ImageView imgHasil;
     CameraView camera;
-    Button btnCapture, btnSend;
+    Button btnCapture, btnSend, btnGallery;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,6 +44,7 @@ public class PostActivity extends AppCompatActivity {
         imgHasil = findViewById(R.id.imageView);
         btnCapture = findViewById(R.id.btnCapture);
         btnSend = findViewById(R.id.btnKirim);
+        btnGallery = findViewById(R.id.btnGallery);
         final DateTimePicker.Builder builder = new DateTimePicker.Builder(this, R.style.Theme_AppCompat);
 
         Calendar date = Calendar.getInstance();
@@ -104,6 +111,34 @@ public class PostActivity extends AppCompatActivity {
         });
 
 
+
+        DialogProperties properties = new DialogProperties();
+        properties.selection_mode = DialogConfigs.SINGLE_MODE;
+        properties.selection_type = DialogConfigs.FILE_SELECT;
+        properties.root = new File(DialogConfigs.DEFAULT_DIR);
+        properties.error_dir = new File(DialogConfigs.DEFAULT_DIR);
+        properties.offset = new File(DialogConfigs.DEFAULT_DIR);
+        properties.extensions = null;
+
+        final FilePickerDialog dialogPicker = new FilePickerDialog(PostActivity.this,properties);
+        dialogPicker.setTitle("Pilih file csv");
+
+        dialogPicker.setDialogSelectionListener(new DialogSelectionListener() {
+            @Override
+            public void onSelectedFilePaths(String[] files) {
+                //files is the array of the paths of files selected by the Application User.
+
+                Picasso.get().load("file://" +files[0]).into(imgHasil);
+            }
+        });
+
+        btnGallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                dialogPicker.show();
+            }
+        });
     }
 
     @Override
